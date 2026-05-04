df229cd92d6c7dbe44f199fdb6233d7c87787d76
MauricioTl23 <teranmauricio22@gmail.com>
2026-05-04 15:22:53 -0400

    Ajustar configuracion SMTP

--- sigire_jm/settings.py
+++ sigire_jm/settings.py
@@ -19,6 +19,8 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv() 
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
@@ -150,9 +152,8 @@ STORAGES = {
     },
 }
 
-BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv() 
+
 
 EMAIL_BACKEND = os.getenv(
     "EMAIL_BACKEND",
@@ -163,6 +164,6 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_TIMEOUT = 20
 
-EMAIL_HOST_USER = os.getenv('EMAIL_USER') 
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASS') 
+EMAIL_HOST_USER = os.getenv("EMAIL_USER", "").strip()
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_PASS", "").strip()
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
